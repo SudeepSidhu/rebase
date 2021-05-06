80cd180ef2976d52c6ef621c871c998ef017112e
Sudeep Sidhu <sudeep.sidhu@neofinancial.com>
2021-05-06 14:07:50 -0600

    squashing commits

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,24 +24,10 @@ AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
           "${URI}/repos/$GITHUB_REPOSITORY/pulls/$PR_NUMBER")
 
+echo "API response: $pr_resp"
+
 BASE_REPO=$(echo "$pr_resp" | jq -r .base.repo.full_name)
 BASE_BRANCH=$(echo "$pr_resp" | jq -r .base.ref)
-
-USER_LOGIN=$(jq -r ".comment.user.login" "$GITHUB_EVENT_PATH")
-
-user_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
-            "${URI}/users/${USER_LOGIN}")
-
-USER_NAME=$(echo "$user_resp" | jq -r ".name")
-if [[ "$USER_NAME" == "null" ]]; then
-	USER_NAME=$USER_LOGIN
-fi
-USER_NAME="${USER_NAME} (Rebase PR Action)"
-
-USER_EMAIL=$(echo "$user_resp" | jq -r ".email")
-if [[ "$USER_EMAIL" == "null" ]]; then
-	USER_EMAIL="$USER_LOGIN@users.noreply.github.com"
-fi
 
 if [[ "$(echo "$pr_resp" | jq -r .rebaseable)" != "true" ]]; then
 	echo "GitHub doesn't think that the PR is rebaseable!"
@@ -59,25 +45,16 @@ HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
 
 echo "Base branch for PR #$PR_NUMBER is $BASE_BRANCH"
 
-USER_TOKEN=${USER_LOGIN//-/_}_TOKEN
-UNTRIMMED_COMMITTER_TOKEN=${!USER_TOKEN:-$GITHUB_TOKEN}
-COMMITTER_TOKEN="$(echo -e "${UNTRIMMED_COMMITTER_TOKEN}" | tr -d '[:space:]')"
-
-git remote set-url origin https://x-access-token:$COMMITTER_TOKEN@github.com/$GITHUB_REPOSITORY.git
-git config --global user.email "$USER_EMAIL"
-git config --global user.name "$USER_NAME"
-
-git remote add fork https://x-access-token:$COMMITTER_TOKEN@github.com/$HEAD_REPO.git
-
 set -o xtrace
 
-# make sure branches are up-to-date
-git fetch origin $BASE_BRANCH
-git fetch fork $HEAD_BRANCH
+git checkout $BASE_BRANCH && git pull
+git checkout $HEAD_BRANCH && git pull
 
-# do the rebase
-git checkout -b $HEAD_BRANCH fork/$HEAD_BRANCH
-git rebase origin/$BASE_BRANCH
+git rebase $HEAD_BRANCH
+git reset --soft $(git rev-parse $BASE_BRANCH) 
 
-# push back
-git push --force-with-lease fork $HEAD_BRANCH
+git add .
+git commit -S -m "Squashed commit"
+
+git push -f
+
